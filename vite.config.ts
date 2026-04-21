0c76690e1025576bef5e4467141ae4aaf2816a78
Raúl Morales Domínguez <raumor@MacBook-Pro-de-Raul.local>
2026-04-21 15:35:14 -0600

    perf: optimize build chunks and split vendor libraries

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,16 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })
